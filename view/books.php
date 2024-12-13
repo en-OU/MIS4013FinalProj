377f5407bac1ea3cab2d63d38684df3c4a89a4aa
en-OU <157330224+en-OU@users.noreply.github.com>
2024-12-13 17:26:27 -0600

    Update books.php

--- view/books.php
+++ view/books.php
@@ -2,10 +2,9 @@
 <html lang="en">
 <head>
 <div style="text-align: center; padding: 50px;">
-  <span style="font-weight: bold; color: blue; font-size: 50px; border-style: dashed; border-radius: 10px; border-width: 2px; display: inline-block; padding: 15px;">Book Catalog</span>
+  <span style="font-weight: bold; color: #006400; font-size: 50px; border-style: dashed; border-radius: 10px; border-width: 2px; display: inline-block; padding: 15px;">Book Catalog</span>
   <br/>
   <br/>
-  <span style="color: violet; font-size: 20px; display: inline-block;">Books</span>
 </div>
 <link rel="stylesheet" href="grid-styles.css">
 </head>
