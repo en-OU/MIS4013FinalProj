f66e027c66829e346f63303c93413f9662418614
en-OU <157330224+en-OU@users.noreply.github.com>
2024-12-12 20:37:27 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,1 +1,13 @@
+<?php
+function get_db_connection(){
+    // Create connection
+    $conn = new mysqli('mis-4013-hw2.mysql.database.azure.com','D3misres', 'ILL92@41', '4013hw3db');
+    
+    // Check connection
+    if ($conn->connect_error) {
+      return false;
+    }
+    return $conn;
+}
 
+?>
