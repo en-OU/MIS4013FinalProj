e717a0144f04ad69ff7991a1bb3b12e4cdf1e292
en-OU <157330224+en-OU@users.noreply.github.com>
2024-12-13 14:57:36 -0600

    Update books.php

--- view/books.php
+++ view/books.php
@@ -29,20 +29,3 @@
    </div>
 </div>
   
-<div class="card-group">
-<?php
-while ($book = $books->fetch_assoc()) {
-?> 
-  <div class="card">
-    <div class="card-body">
-      <h5 class="card-title"> <?php echo htmlspecialchars($book['BookTitle']); ?> </h5>
-      <p class="card-text">
-        <small class="text-body-secondary">Price: $ <?php echo ($book['Price']); ?></small>
-        <small class="text-body-secondary">Rating: <?php echo ($book['Rating']); ?></small>
-      </p>
-    </div>
-  </div>
-<?php
-}
-?>
-</div>
