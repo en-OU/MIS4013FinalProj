be97e550edf79e9c76ada1c91cb1096431116d85
en-OU <157330224+en-OU@users.noreply.github.com>
2024-12-13 17:37:22 -0600

    Update books.php

--- view/books.php
+++ view/books.php
@@ -30,13 +30,6 @@
   <div class="grid-item grid-item--width2"></div>
   <div class="grid-item"></div>
   <div class="grid-item grid-item--height2"></div>
-  <div class="grid-item"></div>
-  <div class="grid-item"></div>
-  <div class="grid-item grid-item--height3"></div>
-  <div class="grid-item grid-item--height2"></div>
-  <div class="grid-item"></div>
-  <div class="grid-item"></div>
-  <div class="grid-item grid-item--height2"></div>
 </div>
 
 <div class="grid" data-masonry='{ "itemSelector": ".grid-item", "columnWidth": 200 }'>
