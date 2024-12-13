14ba906effa3c0e769bcc47211b0357005e13858
en-OU <157330224+en-OU@users.noreply.github.com>
2024-12-13 01:22:23 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,7 @@ $pageTitle = "Home";
 include "header.php";
 ?>
 
-<div style="text-align: center; padding: 50px; background-image: url('bookpile.jpg');> 
+<div style="text-align: center; padding: 50px;> 
   <span style="font-weight: bold; color: pink; font-size: 70px; display: inline-block;">Home</span>
 </br>
-</br>
-  <span style="color: pink; font-size: 50px; display: inline-block;">Homework 5 - Emily Newby</span>
 </div>
