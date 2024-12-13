01072b40787a7c2f5435fe255d849de5cf1c92cb
en-OU <157330224+en-OU@users.noreply.github.com>
2024-12-12 20:37:55 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$pageTitle = "Home";
+include "header.php";
+?>
+
+<div style="text-align: center; padding: 50px; background-image: url('bookpile.jpg');> 
+  <span style="font-weight: bold; color: pink; font-size: 70px; display: inline-block;">Home</span>
+</br>
+</br>
+  <span style="color: pink; font-size: 50px; display: inline-block;">Homework 5 - Emily Newby</span>
+</div>
+
+<?php
+include "footer.php";
+?>
