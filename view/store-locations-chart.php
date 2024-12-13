e97a91f336a307893476d2ff90236be5aee48fb3
en-OU <157330224+en-OU@users.noreply.github.com>
2024-12-13 09:34:21 -0600

    Update store-locations-chart.php

--- view/store-locations-chart.php
+++ view/store-locations-chart.php
@@ -4,3 +4,13 @@
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
      integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
      crossorigin=""></script>
+
+ <div id="map" style="height:180px;"></div>
+
+<script>
+     var map = L.map('map').setView([51.505, -0.09], 13);
+     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     maxZoom: 19,
+     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+     }).addTo(map);
+</script>
