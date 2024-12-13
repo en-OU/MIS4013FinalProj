f04c65a2da14b24d735e8dde5024fc196b333468
en-OU <157330224+en-OU@users.noreply.github.com>
2024-12-13 15:29:49 -0600

    Update books.php

--- view/books.php
+++ view/books.php
@@ -1,17 +1,23 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
 <div style="text-align: center; padding: 50px;">
   <span style="font-weight: bold; color: blue; font-size: 50px; border-style: dashed; border-radius: 10px; border-width: 2px; display: inline-block; padding: 15px;">Book Catalog</span>
   <br/>
   <br/>
   <span style="color: violet; font-size: 20px; display: inline-block;">Books</span>
 </div>
+<link rel="stylesheet" href="grid-styles.css">
+</head>
 
+<body>
 <script src="/path/to/masonry.pkgd.min.js"></script>
 
 <div class="grid" data-masonry='{ "itemSelector": ".grid-item", "columnWidth": 200 }'>
   <?php
     while ($book = $books->fetch_assoc()) {
   ?> 
-  <h5 class="grid-title"> <?php echo ($book['BookTitle']); ?> </h5>
+  <h5 class="grid-title" style="> <?php echo ($book['BookTitle']); ?> </h5>
   <div class="grid-item">
     <small class="text-body-secondary">Author: <?php echo ($book['Author_Name']); ?></small><br>
   </div>
@@ -32,3 +38,6 @@
     }
 ?>
 </div>
+
+</body>
+</html>
