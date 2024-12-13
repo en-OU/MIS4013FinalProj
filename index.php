728e1edea0c8bf19da5a368c71dc8fc49088aefd
en-OU <157330224+en-OU@users.noreply.github.com>
2024-12-13 17:22:31 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ include "header.php";
 ?>
 
 <div style="text-align: center; padding: 50px;> 
-  <span style="font-weight: bold; color: pink; font-size: 70px; display: inline-block;">Home</span>
+  <span style="font-weight: bold; color: #006400; font-size: 70px; display: inline-block;">Home</span>
 </br>
 </div>
