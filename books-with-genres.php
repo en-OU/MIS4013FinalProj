bca5dbd9837302f5268d9bfcb59033a7d3219fe7
en-OU <157330224+en-OU@users.noreply.github.com>
2024-12-13 22:33:27 -0600

    Create books-with-genres.php

---
+++ books-with-genres.php
@@ -0,0 +1,25 @@
+<?php
+// error reporting (from php manual) (I was tired of log stream)
+// https://www.php.net/manual/en/function.error-reporting.php
+// https://www.php.net/manual/en/function.ini-set.php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once("util-db.php");
+require_once("model-books-with-genres.php");
+
+$pageTitle = "Books with Genres";
+include "header.php";
+
+$conn = get_db_connection();
+if (!$conn) 
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$genres = selectGenres($conn);
+
+include "view-books-with-genres.php";
+include "footer.php";
+?>
