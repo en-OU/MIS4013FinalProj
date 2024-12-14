cbef5f2b99e9b4122641941125c7bc938a17dd64
en-OU <157330224+en-OU@users.noreply.github.com>
2024-12-13 19:14:27 -0600

    Update store-locations-chart.php

--- model/store-locations-chart.php
+++ model/store-locations-chart.php
@@ -1,1 +1,13 @@
-
+function selectStore() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("Select StoreID, Address, City, State, Latitude, Longitude from stores;");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
