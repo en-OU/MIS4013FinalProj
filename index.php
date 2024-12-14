84ebcf4f94bb4ff4003cb30703fc925e109214f0
en-OU <157330224+en-OU@users.noreply.github.com>
2024-12-13 22:24:01 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,3 +7,14 @@ include "header.php";
   <span style="font-weight: bold; color: #006400; font-size: 70px; display: inline-block;">Home</span>
 </br>
 </div>
+
+<style>
+        body {
+            background-image: url('Bookpile.jpg');
+            background-size: cover;
+            background-position: center;
+            background-attachment: fixed;
+            margin: 0;
+            background-color: rgba(255, 255, 255, 0.5); 
+        }
+    </style>
