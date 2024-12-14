cf5ccfcf35d2f8b10266caede154a49659b46800
en-OU <157330224+en-OU@users.noreply.github.com>
2024-12-13 23:48:45 -0600

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,20 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once("util-db.php");
+
+$pageTitle = "Cart";
+include "header.php";
+
+$conn = get_db_connection();
+if (!$conn) 
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$books = selectBooks($conn);
+
+include "view/cart.php";
+?>
