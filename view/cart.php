cbdadb440100c6b901a4122f91e7b5bc3a70661d
en-OU <157330224+en-OU@users.noreply.github.com>
2024-12-13 23:47:12 -0600

    Update cart.php

--- view/cart.php
+++ view/cart.php
@@ -1,4 +1,28 @@
-<div class="cart">
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+<div style="text-align: center; padding: 50px;">
+  <span style="background-color: white; font-weight: bold; color: #006400; font-size: 50px; border-style: dashed; border-radius: 10px; border-width: 2px; display: inline-block; padding: 10px;">Cart</span>
+  <br/>
+  <br/>
+</div>
+  
+<link rel="stylesheet" href="grid-styles.css">
+  <style>
+        body {
+            background-image: url('Bookpile.jpg');
+            background-size: cover;
+            background-position: center;
+            background-attachment: fixed;
+            margin: 0;
+            background-color: rgba(255, 255, 255, 0.5); 
+        }
+    </style>
+  
+</head>
+
+<body>
+    <div class="cart">
     <h2>Your Shopping Cart</h2>
     <?php
     if (isset($_SESSION['cart']) && count($_SESSION['cart']) > 0) {
@@ -25,4 +49,6 @@ if (isset($_POST['clear_cart'])) {
     unset($_SESSION['cart']);  // This will clear the cart
 }
 ?>
-
+    
+</body>
+</html>
